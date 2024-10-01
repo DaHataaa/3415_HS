8adce95e46f40864cf92729b07f2f509a26e1e23
BuT9l <122456wsegf@gmail.com>
2024-10-01 14:44:34 +0300

    Убраны массивы в значениях по умолчанию функций + static load_cards() + black

--- src/cards.py
+++ src/cards.py
@@ -6,6 +6,7 @@ cards_repo = "../cards/"
 
 #################################################
 
+
 class Player:
 
     def __init__(self, hp, mp, field, hand, stack):
@@ -21,9 +22,7 @@ class Player:
     def change_dmg(self, delta_dmg):
         self.dmg += delta_dmg
 
-
-
-    '''
+    """
     def attack_with_a_unit(self, pl_slot, en_slot):
         dmg_to_en = self.field.unit_part[pl_slot].dmg
         en = players["p"+str(self.e_id)]
@@ -35,11 +34,12 @@ class Player:
                     print("Перед игроком стоит преграда!")
             case _:
                 en.field.unit_part[en_slot].get_dmg(dmg_to_en)
-    '''
+    """
+
 
 class Field:
 
-    def __init__(self, cards_list=[None]*5):
+    def __init__(self, cards_list=[None] * 5):
         self.cards_list = cards_list
 
     def get_card(self, index):
@@ -47,36 +47,39 @@ class Field:
 
     def place_card(self, card, index):
         self.cards_list[index] = card
-            
+
     def remove_card(self, index):
         self.cards_list[index] = None
 
 
 class Hand(Field):
 
-    def __init__(self, cards_list=[None]*4):
+    def __init__(self, cards_list=None):
+        if cards_list == None:
+            cards_list = [None] * 4
         self.cards_list = cards_list
 
 
 class Stack:
 
-    def __init__(self, cards_list=[]):
+    def __init__(self, cards_list=None):
+        if cards_list == None:
+            cards_list = []
         self.cards_list = cards_list
 
     def get_card(self):
         return self.cards_list[-1]
 
     def push(card):
-        self.cards_list.insert(0,card)
+        self.cards_list.insert(0, card)
 
     def pop():
         return self.cards_list.pop()
 
 
-
-                
 #################################################
-            
+
+
 class Card:
 
     def __init__(self, id, name, fract, mn):
@@ -90,6 +93,30 @@ class Card:
         obj.name = file["name"]
         obj.fract = file["fract"]
         obj.mn = file["mn"]
+
+    @staticmethod
+    def load_cards(cards_repo):
+        cards_list = open(cards_repo + "cards_list.txt").readlines()
+        cards = dict()
+
+        for i in range(len(cards_list)):
+
+            cards_list[i] = cards_list[i].replace("\n", "")
+            card_id = cards_list[i]
+
+            f = json.load(open(cards_repo + card_id + ".json", encoding="utf8"))
+
+            lookup_table = {
+                "unit": Unit.load,
+                "item": Item.load,
+                "location": Location.load,
+                "event": Event.load,
+            }
+
+            card = lookup_table[f["class"]](f)
+            cards[card_id] = card
+
+        return cards, cards_list
 
 
 class Unit(Card):
@@ -117,11 +144,11 @@ class Unit(Card):
             return True
         else:
             return False
-        
-    #def get_dmg(self, dmg, player):
-        #self.hp -= dmg
-        #if self.hp <= 0:
-           #player.field.kill_unit(self)
+
+    # def get_dmg(self, dmg, player):
+    # self.hp -= dmg
+    # if self.hp <= 0:
+    # player.field.kill_unit(self)
 
 
 class Item(Card):
@@ -167,30 +194,6 @@ class Event(Card):
         super().load(obj, file)
         return obj
 
-
-def load_cards(cards_repo):
-
-    cards_list = open(cards_repo + "cards_list.txt").readlines()
-    cards = dict()
-
-    for i in range(len(cards_list)):
-
-        cards_list[i] = cards_list[i].replace("\n", "")
-        card_id = cards_list[i]
-
-        f = json.load(open(cards_repo + card_id + ".json", encoding="utf8"))
-
-        lookup_table = {
-            "unit": Unit.load,
-            "item": Item.load,
-            "location": Location.load,
-            "event": Event.load,
-                        }
-
-        card = lookup_table[f["class"]](f)
-        cards[card_id] = card
-
-    return cards, cards_list
 
 #################################################
 
