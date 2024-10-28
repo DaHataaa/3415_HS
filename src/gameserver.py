0deacb4302dea6760a11d89c8e0f93e6d33150ae
BuT9l <122456wsegf@gmail.com>
2024-10-28 13:29:43 +0300

    Удалил дубликаты функций из GameServer

--- src/gameserver.py
+++ src/gameserver.py
@@ -7,16 +7,3 @@ class GameServer:
         self.atacker = p1
         self.defender = p2
 
-    def attack(self, i_from, i_to):
-        self.defender.change_card_hp(self.attacker.get_dmg(i_from))
-
-    def play_card(self, i_from, i_to):
-        if self.attacker.can_play_card(i_from, i_to):
-            self.attacker.play_card(i_from, i_to)
-
-    def swap_players(self):
-        self.attacker, self.defender = self.defender, self.attacker
-
-    def next_turn(self):
-        self.defender.change_mana()
-        self.swap_players()
