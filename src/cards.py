0d2bc3280e8b32d65353e8edacf07ad2b209c861
BuT9l <122456wsegf@gmail.com>
2024-11-24 19:19:48 +0300

    Улучшил качество кода в cards.py

--- src/cards.py
+++ src/cards.py
@@ -10,40 +10,14 @@ class Card:
         self.mn = mn
 
     def __eq__(self, other):
-        if (
-            self.id == other.id
-            and self.name == other.name
-            and self.fract == other.fract
-            and self.mn == other.mn
-        ):
-            return True
-        return False
+            return self.id == other.id and\
+            self.name == other.name and\
+            self.fract == other.fract and\
+            self.mn == other.mn
 
     @classmethod
     def load(cls, file):
         return cls(id=file["id"], name=file["name"], fract=file["fract"], mn=file["mn"])
-
-    @staticmethod
-    def load_cards(cards_repo):
-        cards_list = open(cards_repo + "cards_list.txt").readlines()
-        cards = dict()
-
-        for i in range(len(cards_list)):
-            cards_list[i] = cards_list[i].replace("\n", "")
-            f = json.load(open(cards_repo + cards_list[i] + ".json", encoding="utf8"))
-            f["id"] = cards_list[i]
-
-            lookup_table = {
-                "unit": Unit.load,
-                "item": Item.load,
-                "location": Location.load,
-                "event": Event.load,
-            }
-
-            card = lookup_table[f["class"]](f)
-            cards[cards_list[i]] = card
-
-        return cards, cards_list
 
 
 class Unit(Card):
@@ -66,35 +40,19 @@ class Unit(Card):
             items=file["items"],
         )
 
-    def __eq__(self, other):
-        if (
-            Card.__eq__(self, other)
-            and self.dmg == other.dmg
-            and self.hp == other.hp
-            and self.items == other.items
-        ):  # todo: fix (lists compair)
-            return True
-        return False
+    def can_recieve_item(self, item):
+        return item.fract == self.fract
 
-    def recieve_item(self, item):
-        if item.fract != self.fract:
-            return False
+    def recieve_item(self, item: Item):
         self.items.append(item)
         self.hp += item.hp_boost
         self.dmg += item.dmg_boost
-        return True
 
     def change_dmg(self, d_dmg):
         self.dmg += d_dmg
 
     def change_hp(self, d_hp):
         self.hp += d_hp
-
-    def get_dmg(self):
-        return self.dmg
-
-    def get_hp(self):
-        return self.hp
 
 
 class Item(Card):
@@ -105,13 +63,9 @@ class Item(Card):
         self.hp_boost = hp_boost
 
     def __eq__(self, other):
-        if (
-            Card.__eq__(self, other)
-            and self.dmg_boost == other.dmg_boost
-            and self.hp_boost == other.hp_boost
-        ):
-            return True
-        return False
+        return Card.__eq__(self, other) and\
+        self.dmg_boost == other.dmg_boost and\
+        self.hp_boost == other.hp_boost
 
     @classmethod
     def load(cls, file):
@@ -132,13 +86,10 @@ class Location(Card):
         self.hp_boost = hp_boost
 
     def __eq__(self, other):
-        if (
-            Card.__eq__(self, other)
-            and self.dmg_boost == other.dmg_boost
-            and self.hp_boost == other.hp_boost
-        ):
-            return True
-        return False
+        return Card.__eq__(self, other) and\
+        self.dmg_boost == other.dmg_boost and\
+        self.hp_boost == other.hp_boost
+            
 
     @classmethod
     def load(cls, file):
@@ -156,36 +107,55 @@ class Event(Card):
     def __init__(self, id, name, fract, mn):
         Card.__init__(self, id=id, name=name, fract=fract, mn=mn)
 
-    def __eq__(self, other):
-        return True
-
     @classmethod
     def load(cls, file):
         return cls(id=file["id"], name=file["name"], fract=file["fract"], mn=file["mn"])
 
 
 class PlayerUnit(Unit):
-    def __init__(self, hp, mp, mana_delta):
-        self.hp = hp
-        self.max_mana, self.current_mana = mp, mp
-        self.mana_delta = mana_delta
-
-    def __eq__(self, other):
-        if (
-            self.hp == other.hp
-            and self.max_mana == other.max_mana
-            and self.current_mana == other.current_mana
-            and self.mana_delta == other.mana_delta
-        ):
-            return True
-        return False
-
+    def __init__(self, id, name, fract, mn, dmg, hp, items):
+        super().__init__(self,
+                         id=id,
+                         name=name,
+                         fract=fract,
+                         mn=mn,
+                         dmg=dmg,
+                         hp=hp,
+                         items=items)
     @classmethod
     def load(cls, file):
-        # return cls() !? чё с init-ом тут творится
-        pass
+        return cls(
+            id=file["id"],
+            name=file["name"],
+            fract=file["fract"],
+            mn=file["mn"],
+            dmg=file["dmg"],
+            hp=file["hp"],
+            items=file["items"],
+        )
 
-    def change_mana(self, md=None):
-        if md is None:
-            md = self.mana_delta
+    def change_mana(self, md):
         self.current_mana += md
+
+@staticmethod
+def load_cards(cards_repo):
+    cards_list = open(cards_repo + "cards_list.txt").readlines()
+    cards = dict()
+
+    for i in range(len(cards_list)):
+        cards_list[i] = cards_list[i].replace("\n", "")
+        f = json.load(open(cards_repo + cards_list[i] + ".json", encoding="utf8"))
+        f["id"] = cards_list[i]
+
+        lookup_table = {
+            "unit": Unit.load,
+            "item": Item.load,
+            "location": Location.load,
+            "event": Event.load,
+        }
+
+        card = lookup_table[f["class"]](f)
+        cards[cards_list[i]] = card
+
+    return cards, cards_list
+
