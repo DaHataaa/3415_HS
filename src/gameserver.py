6399e6983676daf5a8c55d83b51ae9910075935b
BuT9l <122456wsegf@gmail.com>
2024-11-26 08:01:44 +0300

    Немного переписан init GameServer-а, добавлен метод new_game()

--- src/gameserver.py
+++ src/gameserver.py
@@ -1,11 +1,13 @@
-from src.gamestate import *
+from src.gamestate import GameState
+from src.hand import Hand
+from src.field import Field,FieldNames
+from src.stack import Stack
 
 
 class GameServer:
 	def __init__(self, state: GameState):
-		if isinstance(state, GameState):
-			self.state = state
-		else:
-			p1 = Player(Field(), Hand(), Stack())
-			p2 = Player(Field(), Hand(), Stack())
-			state = GameState(p1, p2)
+		self.game_state = state
+
+	@classmethod
+	def new_game(self):
+		p1 = 
